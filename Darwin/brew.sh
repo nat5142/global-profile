d18fe69024e402489de536445a7abab70205c920
nat5142 <nick.tulli95@gmail.com>
2019-07-19 14:26:23 -0400

    These should have been included

--- Darwin/brew.sh
+++ Darwin/brew.sh
@@ -1,6 +1,6 @@
 
 if [[ $(command -v brew) ]] ; then
-  echo "brew installed"
+  echo "brew already installed."
 else
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew install $(cat ./brew_packages.txt)
